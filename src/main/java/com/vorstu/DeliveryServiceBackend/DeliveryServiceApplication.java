3ceb2195d79c5bae0e3ed2ae50796366bc033dc8
NeDonil <svinoukhov03@gmail.com>
2023-10-28 13:11:25 +0300

    refactor(Total): rename application

--- src/main/java/com/vorstu/DeliveryServiceBackend/DeliveryServiceApplication.java
+++ src/main/java/com/vorstu/DeliveryServiceBackend/DeliveryServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendForVorstuDeliveryServiceApplication {
+public class DeliveryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendForVorstuDeliveryServiceApplication.class, args);
+		SpringApplication.run(DeliveryServiceApplication.class, args);
 	}
 
 }
