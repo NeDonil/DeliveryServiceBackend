cb4d23a17f50d3d531565f44bac8c6364bffebd3
Danil Svinoukhov <35741074+NeDonil@users.noreply.github.com>
2023-12-03 20:18:00 +0300

    Feat/order rejections (#5)
    * feat(Total): update order actions
    
    * fix(ActionProcessor): component annotation

--- src/main/java/com/vorstu/DeliveryServiceBackend/controllers/OrderAction.java
+++ src/main/java/com/vorstu/DeliveryServiceBackend/controllers/OrderAction.java
@@ -2,7 +2,9 @@ package com.vorstu.DeliveryServiceBackend.controllers;
 
 public enum OrderAction {
     MAKE,
-    REFUSE,
+    REJECT,
+    ASSEMBLER_REFUSE,
+    COURIER_REFUSE,
     TO_ASSEMBLY,
     TO_ASSEMBLED,
     TO_DELIVERY,
