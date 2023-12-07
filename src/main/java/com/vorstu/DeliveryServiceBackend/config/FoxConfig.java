11a56187822e89a8693e5ed3b6a471e5d4c5b218
Danil Svinoukhov <35741074+NeDonil@users.noreply.github.com>
2023-12-07 22:10:02 +0300

    fix(Total): Add swagger ui (#6)

---
+++ src/main/java/com/vorstu/DeliveryServiceBackend/config/FoxConfig.java
@@ -0,0 +1,39 @@
+package com.vorstu.DeliveryServiceBackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+@EnableWebMvc
+public class FoxConfig implements WebMvcConfigurer {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Delivery Service API",
+                "VorSTU Delivery Service API",
+                "API TOS",
+                "Terms of service",
+                new Contact("Svinoukhov Danil", "www.example.com", "svinoukhov03@gmail.com"),
+                "", "", Collections.emptyList());
+    }
+
+}
