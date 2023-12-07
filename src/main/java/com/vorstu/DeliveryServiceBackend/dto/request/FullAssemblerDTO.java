4f75376e3d28f5d4998a5610c335d29004369020
NeDonil <svinoukhov03@gmail.com>
2023-12-07 16:14:29 +0300

    fix(Employee): no args dto

--- src/main/java/com/vorstu/DeliveryServiceBackend/dto/request/FullAssemblerDTO.java
+++ src/main/java/com/vorstu/DeliveryServiceBackend/dto/request/FullAssemblerDTO.java
@@ -2,9 +2,11 @@ package com.vorstu.DeliveryServiceBackend.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class FullAssemblerDTO {
     private String fio;
     private String email;
